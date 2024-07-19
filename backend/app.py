ce2d3a3688a4becc7062d574097f661a70dd6770
Roan Bahri <roanbahri@Roans-MacBook-Pro.local>
2024-07-19 00:26:47 -0400

    hi

--- backend/app.py
+++ backend/app.py
@@ -1,84 +1,56 @@
 from flask import Flask, request, jsonify, send_from_directory
-import os
-from dotenv import load_dotenv
 from flask_cors import CORS
 import logging
-import time
-from pymongo import MongoClient
-from bson.objectid import ObjectId
+import os
 import datetime
+from pymongo import MongoClient
 
 app = Flask(__name__, static_folder='../frontend/build', static_url_path='')
-CORS(app, resources={r"/*": {"origins": "*"}})  # Enable CORS for all routes
+CORS(app, resources={r"/*": {"origins": "*"}})
 
-# Configure logging
-logging.basicConfig(level=logging.DEBUG)
-
-# Load environment variables
-load_dotenv()
+# MongoDB setup
 mongo_uri = os.getenv("MONGO_URI", "your_mongo_db_uri")
 client = MongoClient(mongo_uri)
 db = client['Cluster0']
 users_collection = db['users']
 registration_attempts_collection = db['registration_attempts']
 
-MAX_REGISTRATIONS_PER_DAY = 3
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
 
 @app.route('/login/', methods=['POST'])
 def login_page():
-    if request.method == 'POST':
-        try:
-            username = request.json['username']
-            password = request.json['password']
-            user = users_collection.find_one({"username": username})
-            if user and user['password'] == password:
-                return {"auth": "success"}
-            return {"auth": "failure"}
-        except Exception as e:
-            logging.error(f"Error during login: {str(e)}")
-            return {"auth": "failure"}
+    try:
+        username = request.json['username']
+        password = request.json['password']
+        user = users_collection.find_one({"username": username})
+        if user and user['password'] == password:
+            return jsonify({"auth": "success"})
+        return jsonify({"auth": "failure"})
+    except Exception as e:
+        logging.error(f"Error during login: {str(e)}")
+        return jsonify({"auth": "failure"})
 
 @app.route('/create_an_account/', methods=['POST'])
 def create_an_account():
-    start_time = time.time()
     try:
         ip_address = request.remote_addr
-        logging.debug(f"IP address: {ip_address}")
-
-        registration_count = count_registration_attempts(ip_address)
-        logging.debug(f"Registration count for IP {ip_address}: {registration_count}")
-
-        if registration_count >= MAX_REGISTRATIONS_PER_DAY:
-            logging.warning("Max registrations per day exceeded.")
-            return {"status": "failure", "message": "Please try again tomorrow"}
-
         username = request.json['username']
         password = request.json['password']
         
-        logging.debug(f"Received username: {username}")
-
         if users_collection.find_one({"username": username}):
-            logging.warning(f"User {username} already exists.")
-            return {"status": "failure", "message": "User already exists."}
+            return jsonify({"status": "failure", "message": "User already exists."})
 
-        users_collection.insert_one({
-            "username": username,
-            "password": password,
-            "selected_classes": [],
-            "custom_options": []
-        })
-        record_registration_attempt(ip_address)
-        logging.info(f"User {username} successfully registered.")
-        return {"status": "success"}
+        users_collection.insert_one({"username": username, "password": password, "selected_classes": [], "custom_options": []})
+        return jsonify({"status": "success"})
     except KeyError:
-        logging.error("Username or password not provided")
-        return {"status": "failure", "message": "Username or password not provided."}
+        return jsonify({"status": "failure", "message": "Username or password not provided."})
     except Exception as e:
-        logging.error(f"Error during registration: {str(e)}")
-        return {"status": "failure", "message": str(e)}
-    finally:
-        end_time = time.time()
-        logging.debug(f"create_an_account took {end_time - start_time} seconds")
+        return jsonify({"status": "failure", "message": str(e)})
 
 @app.route('/getUserClasses/', methods=['POST'])
 def get_user_classes():
@@ -86,13 +58,9 @@ def get_user_classes():
         username = request.json['username']
         user = users_collection.find_one({"username": username})
         if user:
-            return jsonify({
-                "selected_classes": user.get('selected_classes', []),
-                "custom_options": user.get('custom_options', [])
-            })
+            return jsonify({"selected_classes": user.get('selected_classes', []), "custom_options": user.get('custom_options', [])})
         return jsonify({"status": "failure", "message": "User not found"})
     except Exception as e:
-        logging.error(f"Error fetching user classes: {str(e)}")
         return jsonify({"status": "failure", "message": str(e)})
 
 @app.route('/updateUserClasses/', methods=['POST'])
@@ -101,18 +69,10 @@ def update_user_classes():
         username = request.json['username']
         selected_classes = request.json['selected_classes']
         custom_options = request.json['custom_options']
-        user = users_collection.find_one({"username": username})
-        if not user:
-            return {"status": "failure", "message": "User not found"}
-        users_collection.update_one(
-            {"username": username},
-            {"$set": {"selected_classes": selected_classes, "custom_options": custom_options}}
-        )
-        logging.info(f"User {username}'s classes updated")
-        return {"status": "success"}
+        users_collection.update_one({"username": username}, {"$set": {"selected_classes": selected_classes, "custom_options": custom_options}})
+        return jsonify({"status": "success"})
     except Exception as e:
-        logging.error(f"Error updating user classes: {str(e)}")
-        return {"status": "failure", "message": str(e)}
+        return jsonify({"status": "failure", "message": str(e)})
 
 @app.route('/', defaults={'path': ''})
 @app.route('/<path:path>')
@@ -121,22 +81,6 @@ def serve_react_app(path):
         return send_from_directory(app.static_folder, path)
     else:
         return send_from_directory(app.static_folder, 'index.html')
-
-def record_registration_attempt(ip_address):
-    registration_attempts_collection.insert_one({
-        "ip_address": ip_address,
-        "timestamp": datetime.datetime.now()
-    })
-    logging.debug(f"Recorded registration attempt from IP {ip_address}")
-
-def count_registration_attempts(ip_address):
-    today = datetime.datetime.now().date()
-    count = registration_attempts_collection.count_documents({
-        "ip_address": ip_address,
-        "timestamp": {"$gte": datetime.datetime(today.year, today.month, today.day)}
-    })
-    logging.debug(f"Counted {count} registration attempts from IP {ip_address} today")
-    return count
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
